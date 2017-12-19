d87a5696de547e6e378c37ff4dfef45aaa3fb35a
Nur Islam <nurislam0333@gmail.com>
2017-12-20 00:12:17 +0600

    Added public and views folder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
     response.writeHead(200);
-    response.end('Hi Nur Islam!');
+    response.end('object');
 });
 server.listen(8080);
 consol.log('server is running at 8080 port');
